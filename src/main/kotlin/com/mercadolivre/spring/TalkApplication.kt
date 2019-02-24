ffe692e033b0b2d87372155853993739df0aeb54
Pedro Rabello <pedro.rabello@mercadolivre.com>
2019-02-24 18:40:19 -0300

    configuration

--- src/main/kotlin/com/mercadolivre/spring/TalkApplication.kt
+++ src/main/kotlin/com/mercadolivre/spring/TalkApplication.kt
@@ -1,11 +1,2 @@
 package com.mercadolivre.spring
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class TalkApplication
-
-fun main(args: Array<String>) {
-	runApplication<TalkApplication>(*args)
-}
