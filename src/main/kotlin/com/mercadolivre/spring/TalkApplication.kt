e246b1f5a1d38f75105aa9a73b7409fa96b84c63
Pedro Rabello <pedro.rabello@mercadolivre.com>
2019-02-24 18:59:09 -0300

    start in less than 1 sec

--- src/main/kotlin/com/mercadolivre/spring/TalkApplication.kt
+++ src/main/kotlin/com/mercadolivre/spring/TalkApplication.kt
@@ -1,2 +1,18 @@
 package com.mercadolivre.spring
 
+import org.springframework.fu.kofu.web.server
+import org.springframework.fu.kofu.webApplication
+
+fun main() {
+    app.run()
+}
+
+val app = webApplication {
+    server {
+        router {
+            GET("/") {
+                ok().syncBody("SUP!")
+            }
+        }
+    }
+}
