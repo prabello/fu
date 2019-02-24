59c43010fcc830b7c1737d372799a2981fb9280e
Pedro Rabello <pedro.rabello@mercadolivre.com>
2019-02-24 19:03:03 -0300

    test and start working

--- src/test/kotlin/com/mercadolivre/spring/TalkApplicationTests.kt
+++ src/test/kotlin/com/mercadolivre/spring/TalkApplicationTests.kt
@@ -1,16 +1,12 @@
 package com.mercadolivre.spring
 
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
 class TalkApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		app.run(profiles = "test")
 	}
 
 }
